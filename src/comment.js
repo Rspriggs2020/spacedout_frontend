eaced3065bcca13158bd2f5233c1dcdeea6e28cc
ryliespriggs <rspriggs812@gmail.com>
2021-10-05 19:15:13 -0700

    adding edit and delete actions

--- src/comment.js
+++ src/comment.js
@@ -9,18 +9,41 @@ class Comment {
         this.element = document.createElement('li');
         this.element.dataset['id'] = id;
         this.element.id = `comment-${id}`;
+        this.element.addEventListener('click', this.handleClick)
         Comment.all.push(this)
         //debugger
     }
+    //displays title, description, edit and delete
     render() {
         this.element.innerHTML = `
         <div data-id="${this.id}">
         <h1 class="title">${this.title}</h1>
         <p class="description">${this.description}</p>
         </div>
+        <button class="edit" data-id=${this.id}>Edit</button>
+        <button class="delete" data-id=${this.id}>Delete</button>
         `
         return this.element
     }
+    //eventListener for update & delete
+    handleClick = (e) => {
+        if(e.target.innerText === "Edit"){
+            console.log(e.target)
+            this.editForm()
+        }
+        else if(e.target.innerText === "Delete"){
+            console.log()
+            this.deleteForm(e.target)
+        }
+        else if(e.target.innerText === "Save"){
+        }
+    }
+
+    editForm(){
+        const div = this.element.querySelector('div');
+        debugger
+    }
+
 
     attachToDom(){
         Comment.cont.appendChild(this.render())
