884dd1dbd10c5f918874376641f1aa851235ddb8
ryliespriggs <rspriggs812@gmail.com>
2021-10-09 16:10:32 -0700

    changed migrations so changed names of arguments

--- src/info.js
+++ src/info.js
@@ -1,13 +1,13 @@
 class Info {
-    constructor({copyright, date, explanation, hdurl, media_type, image, title, url, id}) {
-        this.copyright = copyright
-        this.date = date
-        this.explanation = explanation
-        this.hdurl = hdurl
-        this.media_type = media_type
+    constructor({image, date, caption, centroid_coordinates, dscovr_j2000_position, lunar_j2000_position, sun_j2000_position, attitude_quaternions, id}) {
         this.image = image
-        this.title = title
-        this.url = url
+        this.date = date
+        this.caption = caption
+        this.centroid_coordinates = centroid_coordinates
+        this.dscovr_j2000_position = dscovr_j2000_position
+        this.lunar_j2000_position = lunar_j2000_position
+        this.sun_j2000_position = sun_j2000_position
+        this.attitude_quaternions = attitude_quaternions
         this.id = id
     }
     addToSelection(){
@@ -15,5 +15,20 @@ class Info {
         option.value = this.id
         option.innerText = this.title
         selection.appendChild(option)
+    }
+
+    renderInfo() {
+        this.element.innerHTML = `
+        <div info-id="${this.id}">
+        <h1 class="image">${this.image}</h1>
+        <p class="date">${this.caption}</p>
+        <p class="date">${this.centroid_coordinates}</p>
+        <p class="date">${this.dscovr_j2000_position}</p>
+        <p class="date">${this.lunar_j2000_position}</p>
+        <p class="date">${this.sun_j2000_position}</p>
+        <p class="date">${this.attitude_quaternions}</p>
+        </div>
+        `
+        return this.element
     }
 }
