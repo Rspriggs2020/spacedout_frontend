1d499c1e0ef1bd763a1e4a182e13a7a05c28975d
ryliespriggs <rspriggs812@gmail.com>
2021-10-09 17:45:55 -0700

    adding more async no function yet

--- src/api_service.js
+++ src/api_service.js
@@ -4,8 +4,26 @@ class ApiService {
     }
 
     async getInfos() {
-        let response = await fetch(this.baseUrl + '/infos')
+        let response = await fetch(this.baseUrl + `/infos`)
         let data = await response.json()
         return data
     }
+
+    async getComments() {
+        let response = await fetch(this.baseUrl + `/comments`)
+        let data = await response.json()
+        return data
+    }
+
+    async getInfo(id) {
+        let response = await fetch(this.baseUrl + `/infos/${id}`)
+        let data = await response.json()
+        return data
+    }
+    async getComment(id) {
+        let response = await fetch(this.baseUrl + `/comments/${id}`)
+        let data = await response.json()
+        return data
+    }
+    
 }
