58878040f89a1b1abc2cd307624228bca82a8ed4
ryliespriggs <rspriggs812@gmail.com>
2021-10-09 18:48:35 -0700

    edititng index html

--- src/index.js
+++ src/index.js
@@ -5,3 +5,12 @@ let data = document.getElementById('data')
 function eventListener() {
     document.getElementById('infos').addEventListener('click')
 }
+
+async function renderInfos() {
+    const infos = await apiService.getInfos()
+    data.innerHTML = ""
+    infos.map(info => {
+        const i = new Info(i)
+        data.innerHTML += i.render() 
+    })
+}
